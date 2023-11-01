23f7aee71247cbc4acdba484ea83a50fd78fb2d7
Remy CASTRO <remycastro27@icloud.com>
2023-11-01 19:36:59 +0100

    Add animation on button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,10 @@ export default function Home() {
                 <div className="mx-auto mb-12 w-full max-w-3xl text-center md:mb-16 lg:mb-20">
                     <h1 className="mb-4 text-[60px] leading-[1.2em] font-semibold text-black dark:text-white ">Comptez les points de vos jeux sur <span className={`bg-cover bg-center px-4 text-white ${styles.bgText}`}>RScores</span>.</h1>
                     <div className="flex justify-center">
-                        <Link href="/games" className="mr-5 flex rounded-xl bg-black px-6  py-4 text-center font-semibold text-white [box-shadow:rgb(51,153,102)_6px_6px] mt-5">{"Créer ma partie"} <MoveRight className={"ml-2"} /></Link>
+                        <Link href="/games" className="mr-5 flex rounded-xl bg-black px-6  py-4 text-center font-semibold text-white [box-shadow:rgb(51,153,102)_6px_6px] mt-5 group duration-150 hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition-all  ease-in-out">
+                            {"Créer ma partie"}
+                            <MoveRight className={"ml-2 transition-all duration-300 group-hover:translate-x-1.5"} />
+                        </Link>
                     </div>
                 </div>
             </section>
